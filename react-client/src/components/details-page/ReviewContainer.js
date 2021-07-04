5a21042e2f296f618dce7c6d1c4c40864b3a8c20
Matthew Thompson <supra__charged@hotmail.com>
2021-07-04 17:13:29 -0600

    Fix review components; add fake review data to Description

--- react-client/src/components/details-page/ReviewContainer.js
+++ react-client/src/components/details-page/ReviewContainer.js
@@ -1,21 +1,25 @@
 import { List } from '@material-ui/core';
 import ReviewItem from './ReviewItem';
 
+import "./ReviewContainer.scss";
+
 const ReviewContainer = props => {
-  const { reviews, avgRating } = props;
+  const { reviews } = props;
 
-  const reviewItems = reviews.map(review => <ReviewItem data={review} />);
+  const reviewItems = reviews.map(review => <ReviewItem data={review} key={review.id} />);
+  const avgRating = reviews.reduce((acc, curr) => curr.rating + acc, 0) / reviews.length;
 
+  // TODO: Need a five-star rating component to display rating
   return (
-    <div>
-      <div>
+    <article className="review-container">
+      <header>
         <span>REVIEWS</span>
         <span>{avgRating}</span>
-      </div>
-      <List className="review-container">
+      </header>
+      <List className="review-container--list">
         {reviewItems}
       </List>
-    </div>
+    </article>
   );
 };
 
--- react-client/src/components/details-page/ReviewItem.js
+++ react-client/src/components/details-page/ReviewItem.js
@@ -1,14 +1,17 @@
-import { ListItem } from "@material-ui/core";
+import { ListItem } from '@material-ui/core';
+
+import './ReviewItem.scss';
 
 const ReviewItem = props => {
-  const { dateReviewed, rating, review } = props;
+  const { image, dateReviewed, rating, review } = props.data;
+  console.log(props);
 
   // TODO: Need a five-star rating component to display rating
   return (
     <ListItem className="review-item">
       <img
         className="review-item__img"
-        src="https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG1lcmNlZGVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        src={image}
         alt="car"
       />
       <div className="review-item__review-container">
