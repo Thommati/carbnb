ae2bea92c5c55e572f61ec6b37895d6d7037f920
Avivit Weissman <wavivit@gmail.com>
2021-07-04 11:54:46 -0700

    added the React router and basic components

--- react-client/src/components/App.js
+++ react-client/src/components/App.js
@@ -1,10 +1,42 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import DetailsPage from "./DetailsPage";
+import SearchResults from "./SearchResults";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Carbnb</h1>
-    </div>
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search-results">Search</Link>
+            </li>
+            <li>
+              <Link to="/deatils-page">Car Details</Link>
+            </li>
+          </ul>
+        </nav> */}
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/deatils-page">
+            <DetailsPage />
+          </Route>
+          <Route path="/search-results">
+            <SearchResults />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
