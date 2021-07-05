e7dd5120fa251950bcf853027da784c1ce2ab6f3
vagrant <hbhandal80@hotmail.com>
2021-07-05 05:04:15 +0000

    Added seeds for locations, availability, orders and reviews

--- express-server/db/seeds/01_users_seeds.sql
+++ express-server/db/seeds/01_users_seeds.sql
@@ -1,6 +1,5 @@
 INSERT INTO users (name, email, phone, hosts, password, image)
-VALUES ('Dominic Parks', 'dominic@carbnb.com', '111 222 3333', FALSE
-, 'qwerty','https://ca.slack-edge.com/T2G8TE2E5-U01UH5DCNR2-a2fb7e763438-512'),
+VALUES ('Dominic Parks', 'dominic@carbnb.com', '111 222 3333', FALSE, 'qwerty','https://ca.slack-edge.com/T2G8TE2E5-U01UH5DCNR2-a2fb7e763438-512'),
 ('Hervinder Bhandal', 'hbhandal80@hotmail.com', '111 777 7777', TRUE, '12345', 'https://media-exp3.licdn.com/dms/image/C5603AQHM8-YeXpBFwA/profile-displayphoto-shrink_800_800/0/1517619190867?e=1631145600&v=beta&t=U4f9fRxNvpCeY9die16rIzk4LEP-HOo7MgILfNDAPZY'),
 ('Matthew Thompson', 'matthew.thompson@hotmail.com', '222 345 6789', TRUE, '12345', 'https://ca.slack-edge.com/T2G8TE2E5-U01UH5DCNR2-a2fb7e763438-512'),
 ('Avivit Weissman', 'wavivit@gmail.com', '333 456 7890', TRUE, '12345', 'https://ca.slack-edge.com/T2G8TE2E5-U01U01QR6KF-130109447a53-512');
---
+++ express-server/db/seeds/06_reviews_seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO reviews(reviewer_id, reviewed_user_id, car_id, date_reviewed, rating, comments)
+VALUES(3, 1, 1, '2021-07-03', 4, 'Some cat hairs on seat, but otherwise a great ride');
