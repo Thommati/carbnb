4ee5a007950f1c530585ff6d92797dc7f3e7bc1d
Matthew Thompson <supra__charged@hotmail.com>
2021-07-05 13:07:51 -0600

    get all cars route can filter by province + city using query params

--- express-server/routes/cars.js
+++ express-server/routes/cars.js
@@ -4,8 +4,24 @@ const db = require('../db');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const query = `SELECT * FROM cars;`;
-  const { rows } = await db.query(query, []);
+  let queryText = `
+  SELECT * FROM cars
+  JOIN users ON cars.user_id = users.id
+  JOIN locations ON locations.user_id = users.id
+  `;
+  const queryParams = [];
+
+  const { province, city } = req.query;
+  if (province && city) {
+    queryText += `
+      WHERE locations.province = $1 AND locations.city = $2
+    `;
+    queryParams.push(province, city);
+  }
+
+  queryText += ';';
+
+  const { rows } = await db.query(queryText, queryParams);
   return res.json(rows);
 });
 
