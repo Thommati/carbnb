7ec05f69d18893eddc03eccfd30a4cff97d1114e
Matthew Thompson <supra__charged@hotmail.com>
2021-07-05 21:57:27 -0600

    Create new user api endpoint returns a JWT

--- express-server/routes/users.js
+++ express-server/routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 
+const { generateJwtToken } = require('../helpers/auth-helpers');
 const { createNewUserAsync } = require('../db/repositories/usersRepo');
 
 const router = express.Router();
 
+// POST /api/users/
+// Creates a new user in DB and returns a JWT
 router.post('/', async (req, res) => {
   try {
     const user = {...req.body};
@@ -13,14 +16,19 @@ router.post('/', async (req, res) => {
     user.password = await bcrypt.hash(req.body.password, 12);
 
     const { rows } = await createNewUserAsync(user);
-    const returnedUser = {
-      id: rows[0].id,
-      name: rows[0].name,
-      email: rows[0].email,
-      image: rows[0].image
-    };
+    const { id, name, email, image } = rows[0];
 
-    return res.status(201).json(returnedUser);
+    // Create JWT token
+    const token = generateJwtToken(id, name, email, image);
+
+    // const returnedUser = {
+    //   id: rows[0].id,
+    //   name: rows[0].name,
+    //   email: rows[0].email,
+    //   image: rows[0].image
+    // };
+
+    return res.status(201).json(token);
   } catch (err) {
     console.log('Error creating new user', err);
     return res.status(500).json({error: 'Internal server error'});
