3a9e0fc777164cc990e91fb65ccb8c507e16d49f
Avivit Weissman <wavivit@gmail.com>
2021-07-07 16:49:02 -0700

    added more places with dummy data and styling

--- react-client/src/components/PopularLocations.js
+++ react-client/src/components/PopularLocations.js
@@ -24,6 +24,13 @@ const useStyles = makeStyles((theme) => ({
     padding: "30px",
     // borderRadius: "8px",
   },
+  h: {
+    borderTop: "2px solid #ddd",
+    maxWidth: "100%",
+    marginTop: "20px",
+    marginBottom: "20px",
+    fontWeight: "bold",
+  },
 }));
 
 function PopularLocations(props) {
@@ -32,6 +39,7 @@ function PopularLocations(props) {
   return (
     <div>
       <Container className={classes.popularLocations} maxWidth="sm">
+        <div className={classes.h}>Popular Locations</div>
         <Grid container spacing={3}>
           <div className={classes.root}>
             <Grid
@@ -66,6 +74,30 @@ function PopularLocations(props) {
             >
               <Avatar alt="Toronto" src={torn} className={classes.large} />
               <div className="place-name">Toronto</div>
+            </Grid>
+            <Grid item xs={6} align="center">
+              <Avatar
+                alt="Victoria"
+                src="https://images.unsplash.com/photo-1589226198562-41f998adf60b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+                className={classes.large}
+              />
+              <div className="place-name">Victoria</div>
+            </Grid>
+            <Grid item xs={6} align="center">
+              <Avatar
+                alt="Edmonton"
+                src="https://images.unsplash.com/photo-1539152620375-ef3504215b05?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZWRtb250b258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                className={classes.large}
+              />
+              <div className="place-name">Edmonton</div>
+            </Grid>
+            <Grid item xs={6} align="center">
+              <Avatar
+                alt="Saskatoon"
+                src="https://images.unsplash.com/photo-1523592131502-9ae85180b9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80"
+                className={classes.large}
+              />
+              <div className="place-name">Saskatoon</div>
             </Grid>
           </div>
         </Grid>
