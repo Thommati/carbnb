24f59cd2086f27f27dce716bce1ff69eb5721bbe
Matthew Thompson <supra__charged@hotmail.com>
2021-07-06 18:55:40 -0600

    Create api endpoint for adding to favourites

--- express-server/db/repositories/favouritesRepo.js
+++ express-server/db/repositories/favouritesRepo.js
@@ -8,3 +8,13 @@ exports.getFavouritesForUserIdAsync = id => {
   const queryParams = [id];
   return db.query(queryText, queryParams);
 };
+
+exports.createNewFavouriteAsync = (userId, carId) => {
+  const queryText = `
+    INSERT INTO favourites (user_id, car_id)
+    VALUES ($1, $2)
+    RETURNING *;
+  `;
+  const queryParams = [userId, carId];
+  return db.query(queryText, queryParams);
+};
--- express-server/routes/favourites.js
+++ express-server/routes/favourites.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const {
-  getFavouritesForUserIdAsync
+  getFavouritesForUserIdAsync,
+  createNewFavouriteAsync,
 } = require ('../db/repositories/favouritesRepo');
 
 const router = express.Router();
 
+// GET /api/favourites/:id
+// Returns all favourties for the user with id
 router.get('/:id', async (req,res) => {
   try {
     const { rows } = await getFavouritesForUserIdAsync(req.params.id);
     return res.json(rows);
   } catch (err) {
     console.log('Error retrieving favourites', err);
+    return res.status(500).json({error: 'Internal server error'});
+  }
+});
+
+// POST /api/favourites
+// Add a car to a user's favourites
+router.post('/', async (req, res) => {
+  const { userId, carId } = req.body;
+  try {
+    const { rows } = await createNewFavouriteAsync(userId, carId);
+    return res.status(201).json(rows[0]);
+  } catch (err) {
+    console.log('Error createing favourite', err);
     return res.status(500).json({error: 'Internal server error'});
   }
 });
