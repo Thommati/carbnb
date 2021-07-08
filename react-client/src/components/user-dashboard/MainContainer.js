adc6eafe4289e901f5884135c670954c57b72852
Hervinder Bhandal <hbhandal80@hotmail.com>
2021-07-08 21:44:40 +0000

    Added email icon and functionality to New Bookings

--- react-client/src/components/user-dashboard/MainContainer.js
+++ react-client/src/components/user-dashboard/MainContainer.js
@@ -14,6 +14,7 @@ import {
   TablePagination,
   TableFooter
 } from '@material-ui/core';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import axios from 'axios';
 
@@ -47,6 +48,10 @@ const useStyles = makeStyles((theme) => ({
   TableFooter: {
     justifyContent: 'right'
   },
+    Mail: {
+    color: '#1e88e5',
+    fontSize: 30
+  },
   Delete: {
     color: '#c62828',
     fontSize: 30
@@ -56,19 +61,6 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(10)
   }
 }));
-
-// function createData(id, image, from, to, make, model, description, price, host, street_number, apartment_number, street, city, province, country, postal_code, status) {
-//   return { id, image, from, to, make, model, description, price, host, street_number, apartment_number, street, city, province, country, postal_code, status };
-// }
-
-// const rows = [
-//   createData(1, 'https://www.thetruthaboutcars.com/wp-content/uploads/2019/11/DSC0778-610x405-1.jpg', '2021-07-06', '2021-07-10', 'Honda', 'Civic', '4 Door', 480, 'Steven Gerrard', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'in progress'),
-//   createData(2, 'https://press.porsche.com/download/prod/presse_pag/PressBasicData.nsf/Download?OpenAgent&attachmentid=1495263&show=1', '2021-07-17', '2021-07-18', 'Porsche', '911s', '2 Door', 500, 'Dominic Crisp', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
-//   createData(3, 'https://s1.1zoom.me/big0/719/Ferrari_488_Spider_Red_Cabriolet_528366_1280x853.jpg', '2021-08-01', '2021-08-03', 'Ferrari', 'Spyder', '2 Door', 2000, 'Hervinder Bhandal', 1125, 401, '12th Avenue', 'Vancouver', 'British Columbia', 'Canada', 'V6H 3Z3', 'upcoming'),
-//   createData(4, 'https://dealerimages.dealereprocess.com/image/upload/1746586.jpg', '2021-09-12', '2021-09-15', 'Ford', 'F150', '4 Door', 900, 'Avivit Weissman', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
-//   createData(5, 'https://i.ytimg.com/vi/X0wOU0F22yE/maxresdefault.jpg', '2021-10-21', '2021-07-22', 'Dodge', 'RAM 1500', '4 Door', 500, 'Matthew Thompson', 110, 'Apt', '9th Avenue SE', 'Calgary', 'Alberta', 'Canada', 'T2G 5A6', 'upcoming'),
-//   createData(6, 'https://images.caricos.com/m/mercedes-benz/2021_mercedes-benz_s-class/images/1024x768/2021_mercedes-benz_s-class_162_1024x768.jpg', '2021-07-10', '2021-07-14', 'Mercedes', 'S Class', '4 Door', 3000, 'Trent Alexander-Arnold', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'cancelled'),
-// ];
 
 const tempUser = 3;
 
@@ -101,6 +93,10 @@ export default function MainContainer() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+  const today = new Date();
+  const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date+' '+time;
 
   return (
     <TableContainer component={Paper} className={classes.TableContainer}>
@@ -114,12 +110,13 @@ export default function MainContainer() {
             <TableCell className={classes.TableHeaderCell} >DATE TO</TableCell>
             <TableCell className={classes.TableHeaderCell} >PRICE</TableCell>
             <TableCell className={classes.TableHeaderCell} >HOST DETAILS</TableCell>
-            <TableCell className={classes.TableHeaderCell} >STATUS</TableCell>
+            <TableCell className={classes.TableHeaderCell} ></TableCell>
             <TableCell className={classes.TableHeaderCell} ></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {orders.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+          row.end_date < dateTime && (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row" align="center">
                 {row.id}
@@ -131,7 +128,6 @@ export default function MainContainer() {
                 <Grid container>
                   <Grid item>
                   <Typography color="primary" variant="subtitle2">{row.make} {row.model}</Typography>
-                    {/* <Typography>{row.description}</Typography> */}
                   </Grid>
                 </Grid>
               </TableCell>
@@ -156,20 +152,13 @@ export default function MainContainer() {
                 </Grid>
               </TableCell>
                 <TableCell>
-                  <Typography
-                    className={classes.status}
-                    style={{
-                      backgroundColor:
-                      ((row.status === 'upcoming' && '#1e88e5') ||
-                      (row.status === 'in progress' && '#9ccc65') ||
-                      (row.status === 'cancelled' && 'red'))
-                    }}
-                  >{row.status}</Typography>
+                  <a href="mailto: {row.email}"><MailOutlineIcon className={classes.Mail}/></a>
                 </TableCell>
                 <TableCell>
                   <DeleteForeverIcon className={classes.Delete}/>
                 </TableCell>
               </TableRow>
+            )
             ))}
           </TableBody>
         </Table>
--- react-client/src/components/user-dashboard/PastBookings.js
+++ react-client/src/components/user-dashboard/PastBookings.js
@@ -53,19 +53,6 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-// function createData(id, image, from, to, make, model, price, host, street_number, street, city, province, country, postal_code, status) {
-//   return { id, image, from, to, make, model, price, host, street_number, street, city, province, country, postal_code, status };
-// }
-
-// const rows = [
-//   createData(1, 'https://www.thetruthaboutcars.com/wp-content/uploads/2019/11/DSC0778-610x405-1.jpg', '2021-07-06', '2021-07-10', 'Honda', 'Civic', '4 Door', 480, 'Steven Gerrard', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'in progress'),
-//   createData(2, 'https://press.porsche.com/download/prod/presse_pag/PressBasicData.nsf/Download?OpenAgent&attachmentid=1495263&show=1', '2021-07-17', '2021-07-18', 'Porsche', '911s', '2 Door', 500, 'Dominic Crisp', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
-//   createData(3, 'https://s1.1zoom.me/big0/719/Ferrari_488_Spider_Red_Cabriolet_528366_1280x853.jpg', '2021-08-01', '2021-08-03', 'Ferrari', 'Spyder', '2 Door', 2000, 'Hervinder Bhandal', 1125, 401, '12th Avenue', 'Vancouver', 'British Columbia', 'Canada', 'V6H 3Z3', 'upcoming'),
-//   createData(4, 'https://dealerimages.dealereprocess.com/image/upload/1746586.jpg', '2021-09-12', '2021-09-15', 'Ford', 'F150', '4 Door', 900, 'Avivit Weissman', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
-//   createData(5, 'https://i.ytimg.com/vi/X0wOU0F22yE/maxresdefault.jpg', '2021-10-21', '2021-07-22', 'Dodge', 'RAM 1500', '4 Door', 500, 'Matthew Thompson', 110, 'Apt', '9th Avenue SE', 'Calgary', 'Alberta', 'Canada', 'T2G 5A6', 'upcoming'),
-//   createData(6, 'https://images.caricos.com/m/mercedes-benz/2021_mercedes-benz_s-class/images/1024x768/2021_mercedes-benz_s-class_162_1024x768.jpg', '2021-07-10', '2021-07-14', 'Mercedes', 'S Class', '4 Door', 3000, 'Trent Alexander-Arnold', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'cancelled'),
-// ];
-
 const tempUser = 3;
 
 export default function PastBookings() {
@@ -97,6 +84,10 @@ export default function PastBookings() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+  const today = new Date();
+  const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date+' '+time;
 
   return (
     <TableContainer component={Paper} className={classes.TableContainer}>
@@ -115,7 +106,8 @@ export default function PastBookings() {
         </TableHead>
         <TableBody>
           {orders.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-            <TableRow key={row.id}>
+          row.end_date > dateTime && (
+          <TableRow key={row.id}>
               <TableCell component="th" scope="row" align="center">
                 {row.id}
               </TableCell>
@@ -153,6 +145,7 @@ export default function PastBookings() {
                   <UserReview />
                 </TableCell>
               </TableRow>
+          )
             ))}
           </TableBody>
         </Table>
