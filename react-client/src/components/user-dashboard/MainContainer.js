0f6a3d3716a081cf8ed7f57f63f381b0331e58c7
Hervinder Bhandal <hbhandal80@hotmail.com>
2021-07-08 13:22:18 +0000

    Added db link for UserAdmin new bookings

--- react-client/src/components/user-dashboard/MainContainer.js
+++ react-client/src/components/user-dashboard/MainContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Table,
@@ -15,6 +15,7 @@ import {
   TableFooter
 } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   table: {
@@ -56,22 +57,40 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function createData(id, image, from, to, make, model, description, price, host, street_number, apartment_number, street, city, province, country, postal_code, status) {
-  return { id, image, from, to, make, model, description, price, host, street_number, apartment_number, street, city, province, country, postal_code, status };
-}
+// function createData(id, image, from, to, make, model, description, price, host, street_number, apartment_number, street, city, province, country, postal_code, status) {
+//   return { id, image, from, to, make, model, description, price, host, street_number, apartment_number, street, city, province, country, postal_code, status };
+// }
 
-const rows = [
-  createData(1, 'https://www.thetruthaboutcars.com/wp-content/uploads/2019/11/DSC0778-610x405-1.jpg', '2021-07-06', '2021-07-10', 'Honda', 'Civic', '4 Door', 480, 'Steven Gerrard', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'in progress'),
-  createData(2, 'https://press.porsche.com/download/prod/presse_pag/PressBasicData.nsf/Download?OpenAgent&attachmentid=1495263&show=1', '2021-07-17', '2021-07-18', 'Porsche', '911s', '2 Door', 500, 'Dominic Crisp', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
-  createData(3, 'https://s1.1zoom.me/big0/719/Ferrari_488_Spider_Red_Cabriolet_528366_1280x853.jpg', '2021-08-01', '2021-08-03', 'Ferrari', 'Spyder', '2 Door', 2000, 'Hervinder Bhandal', 1125, 401, '12th Avenue', 'Vancouver', 'British Columbia', 'Canada', 'V6H 3Z3', 'upcoming'),
-  createData(4, 'https://dealerimages.dealereprocess.com/image/upload/1746586.jpg', '2021-09-12', '2021-09-15', 'Ford', 'F150', '4 Door', 900, 'Avivit Weissman', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
-  createData(5, 'https://i.ytimg.com/vi/X0wOU0F22yE/maxresdefault.jpg', '2021-10-21', '2021-07-22', 'Dodge', 'RAM 1500', '4 Door', 500, 'Matthew Thompson', 110, 'Apt', '9th Avenue SE', 'Calgary', 'Alberta', 'Canada', 'T2G 5A6', 'upcoming'),
-  createData(6, 'https://images.caricos.com/m/mercedes-benz/2021_mercedes-benz_s-class/images/1024x768/2021_mercedes-benz_s-class_162_1024x768.jpg', '2021-07-10', '2021-07-14', 'Mercedes', 'S Class', '4 Door', 3000, 'Trent Alexander-Arnold', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'cancelled'),
-];
+// const rows = [
+//   createData(1, 'https://www.thetruthaboutcars.com/wp-content/uploads/2019/11/DSC0778-610x405-1.jpg', '2021-07-06', '2021-07-10', 'Honda', 'Civic', '4 Door', 480, 'Steven Gerrard', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'in progress'),
+//   createData(2, 'https://press.porsche.com/download/prod/presse_pag/PressBasicData.nsf/Download?OpenAgent&attachmentid=1495263&show=1', '2021-07-17', '2021-07-18', 'Porsche', '911s', '2 Door', 500, 'Dominic Crisp', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
+//   createData(3, 'https://s1.1zoom.me/big0/719/Ferrari_488_Spider_Red_Cabriolet_528366_1280x853.jpg', '2021-08-01', '2021-08-03', 'Ferrari', 'Spyder', '2 Door', 2000, 'Hervinder Bhandal', 1125, 401, '12th Avenue', 'Vancouver', 'British Columbia', 'Canada', 'V6H 3Z3', 'upcoming'),
+//   createData(4, 'https://dealerimages.dealereprocess.com/image/upload/1746586.jpg', '2021-09-12', '2021-09-15', 'Ford', 'F150', '4 Door', 900, 'Avivit Weissman', 999, 'Apt', 'Canada Place', 'Vancouver', 'British Columbia', 'Canada', 'V6C 3B5', 'upcoming'),
+//   createData(5, 'https://i.ytimg.com/vi/X0wOU0F22yE/maxresdefault.jpg', '2021-10-21', '2021-07-22', 'Dodge', 'RAM 1500', '4 Door', 500, 'Matthew Thompson', 110, 'Apt', '9th Avenue SE', 'Calgary', 'Alberta', 'Canada', 'T2G 5A6', 'upcoming'),
+//   createData(6, 'https://images.caricos.com/m/mercedes-benz/2021_mercedes-benz_s-class/images/1024x768/2021_mercedes-benz_s-class_162_1024x768.jpg', '2021-07-10', '2021-07-14', 'Mercedes', 'S Class', '4 Door', 3000, 'Trent Alexander-Arnold', 1655, 'Apt', 'Powick Rd', 'Kelowna', 'British Columbia', 'Canada', 'V1X 4L1', 'cancelled'),
+// ];
+
+const tempUser = 3;
 
 export default function MainContainer() {
   const classes = useStyles();
+  const [orders, setOrders] = useState([]);
   const [page, setPage] = React.useState(0);
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const response = await axios.get(`/api/orders/user/${tempUser}`);
+        if (response.status === 200) {
+          setOrders(response.data);
+          console.log(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getOrders();
+  }, []);
+
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const handleChangePage = (event, newPage) => {
@@ -100,7 +119,7 @@ export default function MainContainer() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+          {orders.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row" align="center">
                 {row.id}
@@ -117,18 +136,18 @@ export default function MainContainer() {
                 </Grid>
               </TableCell>
               <TableCell>
-              <Typography color="textSecondary" variant="body2">{row.from}</Typography>
+              <Typography color="textSecondary" variant="body2">{row.start_date}</Typography>
               </TableCell>
               <TableCell>
-              <Typography color="textSecondary" variant="body2">{row.to}</Typography>
+              <Typography color="textSecondary" variant="body2">{row.end_date}</Typography>
               </TableCell>
               <TableCell>
-                <Typography color="textSecondary" variant="body2">${row.price}</Typography>
+                <Typography color="textSecondary" variant="body2">{row.price}</Typography>
               </TableCell>
               <TableCell>
                 <Grid Container>
                   <Grid item>
-                    <Typography className={classes.name}>{row.host}</Typography>
+                    <Typography className={classes.name}>{row.owners_name}</Typography>
                     <Typography color="textSecondary" variant="body2">{row.street_number} {row.street},</Typography>
                     <Typography color="textSecondary" variant="body2">{row.city},</Typography>
                     <Typography color="textSecondary" variant="body2">{row.province},</Typography>
@@ -158,7 +177,7 @@ export default function MainContainer() {
         <TablePagination
           rowsPerPageOptions={[5, 10, 15]}
           component="div"
-          count={rows.length}
+          count={orders.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onChangePage={handleChangePage}
