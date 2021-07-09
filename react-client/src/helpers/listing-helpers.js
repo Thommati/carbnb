9875eb7483621cad65b32cba119fbb779ac35e50
Matthew Thompson <supra__charged@hotmail.com>
2021-07-09 13:47:01 -0600

    listing helpers determines days to disable for availability

--- react-client/src/helpers/listing-helpers.js
+++ react-client/src/helpers/listing-helpers.js
@@ -1,26 +1,35 @@
-import { isBefore, isAfter } from 'date-fns';
+import { isBefore, addDays } from 'date-fns';
 
 const getMinAndMaxDates = listings => {
   const today = new Date();
-  let minDate = null;
-  let maxDate = today;
+  if (listings.length > 0) {
+    let disabledDates = [];
+    const earliestPossibleStart = new Date(listings[0].start_date);
+    const minDate = isBefore(earliestPossibleStart, today) ? today : earliestPossibleStart;
+    const maxDate = new Date(listings[listings.length - 1].end_date);
 
-  for (const listing of listings ){
-    const start = new Date(listing.start_date);
-    const end = new Date(listing.end_date);
-
-    if (!minDate && isBefore(start, today)) {
-      minDate = today;
-    } else if (!minDate || isBefore(start, minDate)) {
-      minDate = start;
+    if (listings.length > 1) {
+      for (let i = 1; i < listings.length; i++) {
+        let begin = new Date(listings[i - 1].end_date);
+        let end = new Date(listings[i].start_date);
+        disabledDates = [...disabledDates, ...unavailableDays(begin, end)];
+      }
     }
 
-    if (isAfter(end, maxDate)) {
-      maxDate = end;
-    }
+    return { minDate, maxDate, disabledDates };
   }
 
-  return { minDate, maxDate };
+  return { minDate: today, maxDate: today, disabledDates: [today] };
+};
+
+const unavailableDays = (begin, end) => {
+  const unavailable = [];
+  let refDay = addDays(begin, 1);
+  while (isBefore(refDay, end)) {
+    unavailable.push(refDay);
+    refDay = addDays(refDay, 1);
+  }
+  return unavailable;
 };
 
 export {
