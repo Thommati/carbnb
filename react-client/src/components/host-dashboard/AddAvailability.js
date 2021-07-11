e42b5ee43e3147096f472d4bb13f1fb5e50b855c
Matthew Thompson <supra__charged@hotmail.com>
2021-07-11 16:49:00 -0600

    Remove unused imports from AddAvailability component

--- react-client/src/components/host-dashboard/AddAvailability.js
+++ react-client/src/components/host-dashboard/AddAvailability.js
@@ -16,13 +16,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Checkbox from '@material-ui/core/Checkbox';
-import Paper from '@material-ui/core/Paper';
 
-import Select from "@material-ui/core/Select";
 import axios from "axios";
 import { authContext } from "../../providers/authProvider";
 
