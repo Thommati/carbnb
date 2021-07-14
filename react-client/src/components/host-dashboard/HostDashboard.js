ac00861d4fd60e43c02ac8c5def45acec2e20a9b
Hervinder Bhandal <hbhandal80@hotmail.com>
2021-07-13 23:04:53 -0700

    removed the navbar

--- react-client/src/components/host-dashboard/HostDashboard.js
+++ react-client/src/components/host-dashboard/HostDashboard.js
@@ -1,24 +1,9 @@
 import HostTabs from "./HostTabs";
-import SideBarNav from "./SideBarNav";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Grid,
-} from '@material-ui/core';
 
 function HostDashboard() {
   return(
   <div>
-    <div>
-      <SideBarNav />
-    </div>
-    <div>
-      <HostTabs />
-    </div>
+    <HostTabs />
   </div>
   );
 };
