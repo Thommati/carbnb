b49c8051dacab11c8525130296a735dc9359ba36
Matthew Thompson <supra__charged@hotmail.com>
2021-07-14 14:38:54 -0600

    Add two reviews to seeds

--- express-server/db/seeds/06_reviews_seeds.sql
+++ express-server/db/seeds/06_reviews_seeds.sql
@@ -1,2 +1,5 @@
 INSERT INTO reviews(reviewer_id, car_id, date_reviewed, rating, comments)
-VALUES(3, 1, '2021-07-03', 4, 'Some cat hairs on seat, but otherwise a great ride');
+VALUES
+  (3, 1, '2021-07-03', 4, 'Some cat hairs on seat, but otherwise a great ride'),
+  (3,29, '2021-05-17', 5, 'Amazing car, and an excellent experience renting from Hervinder.'),
+  (4,29, '2021-06-16', 5, 'I loved this car.  It was perfect.');
